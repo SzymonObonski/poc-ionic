a3f7dfe31a9c4f654071f12b600d95688098714a
Szymon <sz.obonski@gmail.com>
2016-03-24 10:02:27 +0100

    change path to cordova.js

--- www/js/app.js
+++ www/js/app.js
@@ -94,6 +94,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/go');
+  $urlRouterProvider.otherwise('/tab/map');
 
 });
